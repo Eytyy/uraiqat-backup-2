b9db5be82e2b0aa6f2f375ef6194b91b0b47a194
Eytyy <e.tayyem@gmail.com>
2018-03-18 17:02:01 +0200

    fix images sizes

--- src/client/components/work/ProjectPreviewThumbnails.js
+++ src/client/components/work/ProjectPreviewThumbnails.js
@@ -8,10 +8,10 @@ const ProjectPreviewThumbnails = ({ main, craft, diagram, drawing}) => {
 		return (
 			<div className={`project-preview__thumbs project-preview__thumbs--${orientation}`}>
 				<div className="project-preview__thumbs__left-col">
-					{ main && <ImageComponent imagesQuery={drawingQuery} classList='project-preview__image project-preview__image--main' src={main.fields.file.url} />}
+					{ main && <ImageComponent imagesQuery={'?fl=progressive&w=700&h=416'} classList='project-preview__image project-preview__image--main' src={main.fields.file.url} />}
 				</div>
 				<div className="project-preview__thumbs__right-col">
-					{ drawing && <ImageComponent imagesQuery={'?fl=progressive&w=322&h=416'} classList='project-preview__image project-preview__image--drawing' src={drawing.fields.file.url} />}
+					{ drawing && <ImageComponent imagesQuery={drawingQuery} classList='project-preview__image project-preview__image--drawing' src={drawing.fields.file.url} />}
 					{ diagram && <ImageComponent imagesQuery={'?fl=progressive&w=322&h=208'} classList='project-preview__image project-preview__image--diagram' src={diagram.fields.file.url} />}
 					{ craft && <ImageComponent imagesQuery={'?fl=progressive&w=322&h=208'} classList='project-preview__image project-preview__image--craft' src={craft.fields.file.url} />}
 				</div>
@@ -21,8 +21,8 @@ const ProjectPreviewThumbnails = ({ main, craft, diagram, drawing}) => {
 	return (
 		<div className={`project-preview__thumbs project-preview__thumbs--${orientation}`}>
 			<div className="project-preview__thumbs__left-col">
-				{ main && <ImageComponent imagesQuery={drawingQuery} classList='project-preview__image project-preview__image--main' src={main.fields.file.url} />}
-				{ drawing && <ImageComponent imagesQuery={'?fl=progressive&w=322&h=416'} classList='project-preview__image project-preview__image--drawing' src={drawing.fields.file.url} />}
+				{ main && <ImageComponent imagesQuery={'?fl=progressive&w=700&h=416'} classList='project-preview__image project-preview__image--main' src={main.fields.file.url} />}
+				{ drawing && <ImageComponent imagesQuery={drawingQuery} classList='project-preview__image project-preview__image--drawing' src={drawing.fields.file.url} />}
 			</div>
 			<div className="project-preview__thumbs__right-col">
 				{ diagram && <ImageComponent imagesQuery={'?fl=progressive&w=322&h=208'} classList='project-preview__image project-preview__image--diagram' src={diagram.fields.file.url} />}
