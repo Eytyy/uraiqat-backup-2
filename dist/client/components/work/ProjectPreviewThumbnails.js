6cd1a6970f10b31ff9b863b0783def7a725c2ce9
Eytyy <e.tayyem@gmail.com>
2018-03-18 16:54:54 +0200

    work landing landscape drawing orientation

--- dist/client/components/work/ProjectPreviewThumbnails.js
+++ dist/client/components/work/ProjectPreviewThumbnails.js
@@ -20,13 +20,33 @@ var ProjectPreviewThumbnails = function ProjectPreviewThumbnails(_ref) {
 	    diagram = _ref.diagram,
 	    drawing = _ref.drawing;
 
+	var orientation = drawing.fields.file.details.image.width > drawing.fields.file.details.image.height ? 'landscape' : 'portrait';
+	var drawingQuery = orientation === 'landscape' ? '?fl=progressive&w=644&h=208' : '?fl=progressive&w=322&h=416';
+	if (orientation === 'landscape') {
+		return _react2.default.createElement(
+			'div',
+			{ className: 'project-preview__thumbs project-preview__thumbs--' + orientation },
+			_react2.default.createElement(
+				'div',
+				{ className: 'project-preview__thumbs__left-col' },
+				main && _react2.default.createElement(_ImageComponent2.default, { imagesQuery: drawingQuery, classList: 'project-preview__image project-preview__image--main', src: main.fields.file.url })
+			),
+			_react2.default.createElement(
+				'div',
+				{ className: 'project-preview__thumbs__right-col' },
+				drawing && _react2.default.createElement(_ImageComponent2.default, { imagesQuery: '?fl=progressive&w=322&h=416', classList: 'project-preview__image project-preview__image--drawing', src: drawing.fields.file.url }),
+				diagram && _react2.default.createElement(_ImageComponent2.default, { imagesQuery: '?fl=progressive&w=322&h=208', classList: 'project-preview__image project-preview__image--diagram', src: diagram.fields.file.url }),
+				craft && _react2.default.createElement(_ImageComponent2.default, { imagesQuery: '?fl=progressive&w=322&h=208', classList: 'project-preview__image project-preview__image--craft', src: craft.fields.file.url })
+			)
+		);
+	}
 	return _react2.default.createElement(
 		'div',
-		{ className: 'project-preview__thumbs' },
+		{ className: 'project-preview__thumbs project-preview__thumbs--' + orientation },
 		_react2.default.createElement(
 			'div',
 			{ className: 'project-preview__thumbs__left-col' },
-			main && _react2.default.createElement(_ImageComponent2.default, { imagesQuery: '?fl=progressive&w=700&h=416', classList: 'project-preview__image project-preview__image--main', src: main.fields.file.url }),
+			main && _react2.default.createElement(_ImageComponent2.default, { imagesQuery: drawingQuery, classList: 'project-preview__image project-preview__image--main', src: main.fields.file.url }),
 			drawing && _react2.default.createElement(_ImageComponent2.default, { imagesQuery: '?fl=progressive&w=322&h=416', classList: 'project-preview__image project-preview__image--drawing', src: drawing.fields.file.url })
 		),
 		_react2.default.createElement(
--- src/client/components/work/ProjectPreviewThumbnails.js
+++ src/client/components/work/ProjectPreviewThumbnails.js
@@ -2,10 +2,26 @@ import React from 'react';
 import ImageComponent from '../media/ImageComponent';
 
 const ProjectPreviewThumbnails = ({ main, craft, diagram, drawing}) => {
+	const orientation = drawing.fields.file.details.image.width > drawing.fields.file.details.image.height ? 'landscape' : 'portrait';
+	const drawingQuery = orientation === 'landscape' ? '?fl=progressive&w=644&h=208' : '?fl=progressive&w=322&h=416';
+	if (orientation === 'landscape') {
+		return (
+			<div className={`project-preview__thumbs project-preview__thumbs--${orientation}`}>
+				<div className="project-preview__thumbs__left-col">
+					{ main && <ImageComponent imagesQuery={drawingQuery} classList='project-preview__image project-preview__image--main' src={main.fields.file.url} />}
+				</div>
+				<div className="project-preview__thumbs__right-col">
+					{ drawing && <ImageComponent imagesQuery={'?fl=progressive&w=322&h=416'} classList='project-preview__image project-preview__image--drawing' src={drawing.fields.file.url} />}
+					{ diagram && <ImageComponent imagesQuery={'?fl=progressive&w=322&h=208'} classList='project-preview__image project-preview__image--diagram' src={diagram.fields.file.url} />}
+					{ craft && <ImageComponent imagesQuery={'?fl=progressive&w=322&h=208'} classList='project-preview__image project-preview__image--craft' src={craft.fields.file.url} />}
+				</div>
+			</div>
+		);
+	}
 	return (
-		<div className="project-preview__thumbs">
+		<div className={`project-preview__thumbs project-preview__thumbs--${orientation}`}>
 			<div className="project-preview__thumbs__left-col">
-				{ main && <ImageComponent imagesQuery={'?fl=progressive&w=700&h=416'} classList='project-preview__image project-preview__image--main' src={main.fields.file.url} />}
+				{ main && <ImageComponent imagesQuery={drawingQuery} classList='project-preview__image project-preview__image--main' src={main.fields.file.url} />}
 				{ drawing && <ImageComponent imagesQuery={'?fl=progressive&w=322&h=416'} classList='project-preview__image project-preview__image--drawing' src={drawing.fields.file.url} />}
 			</div>
 			<div className="project-preview__thumbs__right-col">
