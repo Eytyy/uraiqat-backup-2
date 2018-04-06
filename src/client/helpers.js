9e3553be6f6dcc64d7e32fc7b59d7ea812ac11ba
Eytyy <e.tayyem@gmail.com>
2018-04-06 20:48:51 +0300

    work landing patterns config

--- src/client/helpers.js
+++ src/client/helpers.js
@@ -57,89 +57,203 @@ export const getFontValues = () => {
 	return font;
 };
 
-export const getContainerSize = (container) => {
-	const containerSize = {
-		w: container.w,
-		h: container.h
-	};
+const config = {
+	mobile: {
+		'project-main-thumb': {
+			w: 280,
+			h: 168,
+		},
+		'project-drawing-thumb--portrait': {
+			w: 140,
+			h: 192,
+		},
+		'project-drawing-thumb--landscape': {
+			w: 280,
+			h: 96,
+		},
+		'project-other-thumb': {
+			w: 140,
+			h: 96,
+		},
+	},
+	mobileX2: {
+		'project-main-thumb': {
+			w: 320,
+			h: 168,
+		},
+		'project-drawing-thumb--portrait': {
+			w: 160,
+			h: 192,
+		},
+		'project-drawing-thumb--landscape': {
+			w: 320,
+			h: 96,
+		},
+		'project-other-thumb': {
+			w: 160,
+			h: 96,
+		},
+	},
+	tablet: {
+		'project-main-thumb': {
+			w: 384,
+			h: 260,
+		},
+		'project-drawing-thumb--portrait': {
+			w: 168,
+			h: 260,
+		},
+		'project-drawing-thumb--landscape': {
+			w: 336,
+			h: 130,
+		},
+		'project-other-thumb': {
+			w: 168,
+			h: 130,
+		},
+	},
+	desktop: {
+		'project-main-thumb': {
+			w: 532,
+			h: 320,
+		},
+		'project-drawing-thumb--portrait': {
+			w: 210,
+			h: 320,
+		},
+		'project-drawing-thumb--landscape': {
+			w: 420,
+			h: 160,
+		},
+		'project-other-thumb': {
+			w: 210,
+			h: 160,
+		},
+	},
+	desktop1280: {
+		'project-main-thumb': {
+			w: 574,
+			h: 384,
+		},
+		'project-drawing-thumb--portrait': {
+			w: 294,
+			h: 384,
+		},
+		'project-drawing-thumb--landscape': {
+			w: 588,
+			h: 192,
+		},
+		'project-other-thumb': {
+			w: 294,
+			h: 192,
+		},
+	},
+	desktop1440: {
+		'default-post--portrait': {
+			w: 266,
+			h: 384,
+		},
+		'default-post--landscape': {
+			w: 392,
+			h: 256,
+		},
+		'featured-post--portrait': {
+			w: 476,
+			h: 672,
+		},
+		'featured-post--landscape': {
+			w: 882,
+			h: 576,
+		},
+		'featured-post--video': {
+			w: 868,
+			h: 544,
+		},
+		'default-post--video': {
+			w: 630,
+			h: 352,
+		},
+		'post-slider': {
+			w: 882,
+			h: 576,
+		},
+		'post-media--video': {
+			w: 900,
+			h: 512,
+		},
+		'post-media--image': {
+			w: 900,
+			h: 512
+		},
+		'project-main-thumb': {
+			w: 700,
+			h: 416,
+		},
+		'project-drawing-thumb--portrait': {
+			w: 322,
+			h: 416,
+		},
+		'project-drawing-thumb--landscape': {
+			w: 644,
+			h: 208,
+		},
+		'project-other-thumb': {
+			w: 322,
+			h: 208,
+		},
+		'project-main-slider': {
+			w: 826,
+			h: 512,
+		},
+		'project-drawings-slider': {
+			w: 334,
+			h: 254,
+		},
+		'related-project--landscape': {
+			w: 644,
+			h: 448
+		},
+	},
+	desktop1600: {
 
-	containerSize.w = containerSize.w;
-	containerSize.h = containerSize.h;
+	}
+};
+export const getContainerSize = (container, preConfig) => {
+	let containerSize = {};
 
+	if (container === 'custom') {
+		containerSize.w = preConfig.w;
+		containerSize.h = preConfig.h;
+
+		return containerSize;
+	}
+
+	const windowDimensions = getWindowDimensions();
+	let configTarget;
+	
+	if (windowDimensions.w >= 1600) {
+		configTarget = config['desktop1600'];
+	} else if (windowDimensions.w >= 1440) {
+		configTarget = config['desktop1440'];
+	} else if (windowDimensions.w >= 1280) {
+		configTarget = config['desktop1280'];
+	} else if (windowDimensions.w >= 1024) {
+		configTarget = config['desktop'];
+	} else if (windowDimensions.w >= 768 ) {
+		configTarget = config['tablet'];
+	} else if (windowDimensions.w >= 414 ) {
+		configTarget = config['mobileX2'];
+	} else {
+		configTarget = config['mobile'];
+	}
+	containerSize.w = configTarget[container].w;
+	containerSize.h = configTarget[container].h;
 	return containerSize;
 };
 
 
-const config = {
-	'project-main-slider': {
-		w: 826,
-		h: 512,
-	},
-	'project-drawings-slider': {
-		w: 334,
-		h: 254,
-	},
-	'post-slider': {
-		w: 882,
-		h: 576,
-	},
-	'default-post--portrait': {
-		w: 266,
-		h: 384,
-	},
-	'default-post--landscape': {
-		w: 392,
-		h: 256,
-	},
-	'featured-post--portrait': {
-		w: 476,
-		h: 672,
-	},
-	'featured-post--landscape': {
-		w: 882,
-		h: 576,
-	},
-	'featured-post--video': {
-		w: 868,
-		h: 544,
-	},
-	'default-post--video': {
-		w: 630,
-		h: 352,
-	},
-	'related-project--landscape': {
-		w: 644,
-		h: 448
-	},
-	'project-main-thumb': {
-		w: 700,
-		h: 416,
-	},
-	'project-drawing-thumb--landscape': {
-		w: 644,
-		h: 208,
-	},
-	'project-drawing-thumb--portrait': {
-		w: 322,
-		h: 416,
-	}
-	,
-	'project-other-thumb': {
-		w: 322,
-		h: 208,
-	},
-	'post-media--video': {
-		w: 900,
-		h: 512,
-	},
-	'post-media--image': {
-		w: 900,
-		h: 512
-	}
-};
-
-export const getNoOfChars = (sliderName, preconfig) => {
-	const containerSize = typeof preconfig === 'undefined' ? getContainerSize(config[sliderName]) : getContainerSize(preconfig);
+export const getNoOfChars = (container, preconfig) => {
+	const containerSize = typeof preconfig === 'undefined' ? getContainerSize(container) : getContainerSize('custom', preconfig);
 	const font = getFontValues();
 
 	const x = Math.floor(containerSize.w/font.characterWidth) + font.extra;
